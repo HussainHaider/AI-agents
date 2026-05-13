4ff175e512f9c09363461e25923ffa2ef054bff0
HussainHaider <hussainhaider490@gmail.com>
2026-05-13 05:44:39 +0200

    feat: add various chat models and embedding functionalities

---
+++ langChain/models/chat_models/chatmodel_anthropic.py
@@ -0,0 +1,8 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatAnthropic(model='claude-3-5-sonnet-20241022', temperature=1.5, max_tokens=10)
+result = model.invoke("Write a poem on cricket.")
+print(result.content)
---
+++ langChain/models/embedding_models/document_similarity.py
@@ -0,0 +1,35 @@
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+
+documents = [
+    "Babar Azam is a Pakistani cricketer known for his elegant batting and consistent performances across formats.",
+    "Shaheen Shah Afridi is a Pakistani fast bowler famous for his pace, swing, and wicket-taking ability.",
+    "Mohammad Rizwan is a Pakistani wicketkeeper-batsman known for his hard work, consistency, and match-winning innings.",
+    "Wasim Akram is a legendary Pakistani fast bowler regarded as one of the greatest swing bowlers in cricket history.",
+    "Imran Khan is a former Pakistani captain who led Pakistan to victory in the 1992 Cricket World Cup.",
+    "Inzamam-ul-Haq was a Pakistani batsman known for his calm temperament and powerful stroke play.",
+    "Saeed Anwar was a stylish Pakistani opener famous for his fluent batting and high scores in ODI cricket.",
+    "Younis Khan is a former Pakistani batsman who holds the record for the most Test runs by a Pakistani player.",
+    "Shahid Afridi was a Pakistani all-rounder known for his explosive batting and entertaining style of play.",
+    "Abdul Qadir was a legendary Pakistani leg-spinner who played a major role in popularizing leg-spin bowling."
+]
+
+query = 'tell me about Afridi'
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embeddings = embedding.embed_query(query)
+
+scores = cosine_similarity([query_embeddings], doc_embeddings)[0]
+print(scores)
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is:", score)
